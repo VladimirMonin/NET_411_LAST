1929da00214587e6e9cce2babcc067ca65f9634f
VladimirMonin <moninvladimir88@gmail.com>
2026-09-13 14:26:32 +0500

    tests: Проверка функции write_file на реальное создание и запись в файл (а так же проверка содержимого что записалось туда)

--- tests/unit_utils_test.py
+++ tests/unit_utils_test.py
@@ -1,5 +1,6 @@
-from utils.file_utils import read_file
+from utils.file_utils import read_file, write_file
 import pytest
+import os
 
 ASSET_READ_FILE = "tests/assets/read_file_asset.md"
 
@@ -9,8 +10,17 @@ def test_file_read():
     assert content == "Что такое переменная в Python?", "Содержимое не соответствует файлу"
 
 
-# Проверим что появляется ошибка на чтение несуществующего файла
-
 def test_file_read_not_found():
      with pytest.raises(FileNotFoundError):
         read_file("nonexistent_file.txt")  # Попытка прочитать несуществующий файл
+
+
+def test_write_file():
+    content_to_write = read_file(ASSET_READ_FILE)
+    write_file("test_write_file.txt", content_to_write)
+    assert "test_write_file.txt" in os.listdir(), "Файл не создан"
+
+    read_content = read_file("test_write_file.txt")
+    assert read_content == content_to_write, "Содержимое файла не соответствует оригиналу"
+    os.remove("test_write_file.txt")  # Удаляем созданный файл
+
